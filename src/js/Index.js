722fe8baa9477bb28a2595198a9323e6b2dda769
karpi <karpinskiyivann@gmail.com>
2019-09-27 23:20:05 +0300

    test

--- src/js/Index.js
+++ src/js/Index.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import css from './../sass/app.css';
+import FacebookLogo from './icons/facebookLogo.js';
 
 const Index = () => {
     return (
-        <div>
-            zdarova
-            <br/>
-            nu privet
+        <div className='main-wrapper'>
+            <section className='left-side'>
+                <h1>Рекламные баннеры</h1>
+            </section>
+            <section className='middle-side'>
+                <h1>Новости нах</h1>
+                <div className='post'>
+                    <div className='avatar'>
+                        <FacebookLogo/>
+                    </div>
+                    <div className='name'>Толян</div>
+                    <div className='text'>Я воняю</div>
+                </div>
+            </section>
+            <section className='right-side'>
+                <h1>Фильтры</h1>
+            </section>
         </div>
     );
 };
