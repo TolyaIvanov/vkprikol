0edd52a97bbaefd530fc653df7051ca08a5f580a
karpi <karpinskiyivann@gmail.com>
2019-09-28 04:55:36 +0300

    the loopa

--- src/js/components/Settings.js
+++ src/js/components/Settings.js
@@ -1,9 +1,6 @@
 import React, {Component} from 'react';
+import Magnifier from "../icons/Magnifier";
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a88cbb69fe6346ccd199df490c26f45630e45846
 class Settings extends Component {
     constructor(props) {
         super(props);
@@ -16,7 +13,21 @@ class Settings extends Component {
                     <input className="apple-switch" type="checkbox"/>
                     <span className="list-mode-text">Включить отображение списком</span>
                 </div>
-				<input className='search-input'/>
+				<label className='tags-title'>Выберите популярные хэштеги:</label>
+                <div className='tags-area'>
+                    <div className='tag'>#хуета</div>
+                    <div className='tag'>#хуета2</div>
+                    <div className='tag'>#хуееееееета</div>
+                    <div className='tag'>#хуеа</div>
+                    <div className='tag'>#хуета</div>
+                    <div className='tag'>#х1</div>
+                    <div className='tag'>#хааауета</div>
+                    <div className='tag'>#хйета</div>
+                    <div className='tag'>#хуа</div>
+                    <div className='tag'>#хупырфывшгета</div>
+                </div>
+                <input placeholder="Введите имя пользователя" className='search-input'/>
+                <Magnifier/>
             </section>
         );
     }
